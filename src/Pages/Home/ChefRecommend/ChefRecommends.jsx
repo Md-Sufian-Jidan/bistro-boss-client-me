593813e8a244666c027bb76aba117c2e339869d5
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-05-17 12:19:28 +0600

    call us section added

--- src/Pages/Home/ChefRecommend/ChefRecommends.jsx
+++ src/Pages/Home/ChefRecommend/ChefRecommends.jsx
@@ -1,9 +1,18 @@
+import useMenu from "../../../Hooks/useMenu";
 import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
+import ChefCard from "./ChefCard";
 
 const ChefRecommends = () => {
+    const { menu } = useMenu();
+    const chefsRecommends = menu?.filter(recommend => recommend.category === "popular")
     return (
-        <div>
+        <div className="my-5">
             <SectionTitle heading="CHEF RECOMMENDS" subHeading="---Should Try---" />
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-5">
+                {
+                    chefsRecommends?.map(chef => <ChefCard key={chef._id} item={chef}></ChefCard>)
+                }
+            </div>
         </div>
     );
 };
