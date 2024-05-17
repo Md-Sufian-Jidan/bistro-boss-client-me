faf3c3d93f26f29ce2aef136949104fd9fe17f1b
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-05-17 12:47:47 +0600

    chef recommends items card section added

--- src/Pages/Home/Featured/FeaturedBanner.jsx
+++ src/Pages/Home/Featured/FeaturedBanner.jsx
@@ -1,8 +1,8 @@
 import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
 import PropTypes from 'prop-types'
-const FeaturedBanner = ({featuredImg}) => {
+const FeaturedBanner = ({ featuredImg }) => {
     return (
-        <div className="featured-item bg-fixed text-white pt-8 my-20">
+        <div className="featured-item bg-fixed text-white pt-8 my-20" style={{ backgroundImage: `url(${featuredImg})` }}>
             <SectionTitle subHeading="Check it Out" heading="Featured Item"></SectionTitle>
             <div className="md:flex justify-center items-center pb-20 pt-12 px-36 bg-opacity-40 bg-gray-400">
                 <div>
@@ -11,8 +11,7 @@ const FeaturedBanner = ({featuredImg}) => {
                 <div className="md:ml-10 space-y-3">
                     <p>Aug 20, 2024</p>
                     <p className="uppercase">Where Can i get some?</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit recusandae excepturi earum facilis ullam qui dicta quisquam voluptates cum a dolores, tempore quos, nihil similique reprehenderit est veritatis alias rerum. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat porro, officiis eum natus explicabo maxime esse? Delectus, nemo. Nobis tempore odio a atque, voluptate ratione laborum porro assumenda enim id!
-                    </p>
+                    <p>Discover a symphony of flavors in every dish. Our menu offers a delectable selection of gourmet delights, from succulent appetizers to exquisite main courses and decadent desserts. Each plate is a masterpiece, meticulously crafted to tantalize your taste buds. Dive into our culinary offerings and savor the extraordinary today!</p>
                     <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
                 </div>
             </div>
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -5,7 +5,8 @@ import homeCover from '../../../assets/home/banner.jpg'
 import FromOurMenu from "../FromOurMenu/FromOurMenu";
 import CallUs from "../CallUs/CallUs";
 import ChefRecommends from "../ChefRecommend/ChefRecommends";
-
+import FeaturedBanner from "../Featured/FeaturedBanner";
+import featureImg from '../../../assets/home/featured.jpg'
 const Home = () => {
     return (
         <div>
@@ -15,6 +16,7 @@ const Home = () => {
             <FromOurMenu />
             <CallUs />
             <ChefRecommends />
+            <FeaturedBanner featuredImg={featureImg} />
         </div>
     );
 };
