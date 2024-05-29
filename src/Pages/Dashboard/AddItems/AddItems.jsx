5f8e776088c9def8094f9b2c8a0b29b84edfce1e
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-05-29 07:52:39 +0600

    admin home added

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -40,13 +40,12 @@ const AddItems = () => {
                             <div>
                                 <label className="text-gray-700 dark:text-gray-200" htmlFor="username">Recipe Name*</label>
                                 <input {...register("recipeName", { required: true })} id="username" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
-                                {errors.recipeName && <span className="text-red-500">This field is required</span>}
+                                {errors.recipeName && <span className="text-red-500">Recipe Name is required</span>}
                             </div>
 
                             <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
                                 <div>
                                     <label className="text-gray-700 dark:text-gray-200" htmlFor="emailAddress">Category*</label>
-                                    {/* <input id="emailAddress" type="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" /> */}
                                     <select  {...register("categoryValue", { required: true })} name='category' id="" className="w-full px-4 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" >
                                         <option>Select Your category <FaDownLeftAndUpRightToCenter /> </option>
                                         {
@@ -54,7 +53,7 @@ const AddItems = () => {
                                                 <option key={category}>{category}</option>
                                             ))
                                         }
-                                        {errors.categoryValue && <span className="text-red-500">This field is required</span>}
+                                        {errors.categoryValue && <span className="text-red-500">Category is required</span>}
 
                                     </select>
                                 </div>
@@ -62,14 +61,14 @@ const AddItems = () => {
                                 <div>
                                     <label className="text-gray-700 dark:text-gray-200" htmlFor="password">Recipe Price*</label>
                                     <input {...register("recipePrice", { required: true })} id="password" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
-                                    {errors.recipePrice && <span className="text-red-500">This field is required</span>}
+                                    {errors.recipePrice && <span className="text-red-500">Recipe Price is required</span>}
                                 </div>
                             </div>
 
                             <div>
                                 <label className="text-gray-700 dark:text-gray-200" htmlFor="passwordConfirmation">Recipe Description</label>
                                 <textarea {...register("recipeDescription", { required: true })} rows={5} id="passwordConfirmation" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"></textarea>
-                                {errors.recipeDescription && <span className="text-red-500">This field is required</span>}
+                                {errors.recipeDescription && <span className="text-red-500">Recipe Description is required</span>}
                             </div>
                         </div>
 
---
+++ src/Pages/Dashboard/AdminHome/AdminChart.jsx
@@ -0,0 +1,124 @@
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Legend } from 'recharts';
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
+const data = [
+    {
+        name: 'Page A',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Page B',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: 'Page C',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: 'Page D',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: 'Page E',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+];
+
+const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+    Z`;
+};
+
+const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
+// pie chart imports here
+import { PieChart, Pie, ResponsiveContainer } from 'recharts';
+
+const pie = [
+    { name: 'Group A', value: 400 },
+    { name: 'Group B', value: 300 },
+    { name: 'Group C', value: 300 },
+    { name: 'Group D', value: 200 },
+];
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+        <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+            {`${(percent * 100).toFixed(0)}%`}
+        </text>
+    );
+};
+const AdminChart = () => {
+    return (
+        <div className='flex my-5'>
+            {/* bar chart here? */}
+            <div className="w-1/2">
+                <BarChart
+                    width={500}
+                    height={300}
+                    data={data}
+                    margin={{
+                        top: 20,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Bar dataKey="uv" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                        {data.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+                        ))}
+                    </Bar>
+                </BarChart>
+            </div>
+            {/* pie chart here? */}
+            <div className="w-1/2">
+                <ResponsiveContainer width="100%" height="100%">
+                    <PieChart width={400} height={400}>
+                        <Pie
+                            data={pie}
+                            cx="50%"
+                            cy="50%"
+                            labelLine={false}
+                            label={renderCustomizedLabel}
+                            outerRadius={80}
+                            fill="#8884d8"
+                            dataKey="value"
+                        >
+                            {data.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                            ))}
+                        </Pie>
+                        <Legend></Legend>
+                    </PieChart>
+                </ResponsiveContainer>
+            </div>
+        </div>
+    );
+};
+
+export default AdminChart;
