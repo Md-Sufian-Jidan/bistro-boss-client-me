ca20f74c69cc4dcc95513baed570f90248e4e182
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-05-30 09:43:29 +0600

    implement the check out form in payment

--- src/Pages/Dashboard/Payment/CheckOutFrom.jsx
+++ src/Pages/Dashboard/Payment/CheckOutFrom.jsx
@@ -1,9 +1,63 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 
 const CheckOutFrom = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+
+    // handle form submit
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        //check stripe and elements exist . if this things do not exist return
+        if (!stripe || !elements) {
+            return
+        }
+        // Get a reference to a mounted CardElement. Elements knows how
+        // to find your CardElement because there can only ever be one of
+        // each type of element.
+        const card = elements.getElement(CardElement);
+
+        // if card is null return
+        if (card === null) {
+            return
+        }
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        })
+        if (error) {
+            console.log('payment error', error);
+        }
+        else {
+            console.log('payment method', paymentMethod);
+        }
+        
+    };
     return (
-        <div>
-            
-        </div>
+        <form onSubmit={handleSubmit}>
+            <CardElement
+                options={{
+                    style: {
+                        base: {
+                            fontSize: '16px',
+                            color: '#424770',
+                            '::placeholder': {
+                                color: '#aab7c4',
+                            },
+                        },
+                        invalid: {
+                            color: '#9e2146',
+                        },
+                    },
+                }}
+            />
+            <div className="flex justify-center mt-5">
+                <button className="btn btn-success w-full lg:max-w-sm" type="submit" disabled={!stripe}>
+                    Pay
+                </button>
+            </div>
+        </form>
     );
 };
 
--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,9 +1,10 @@
 import { Elements } from "@stripe/react-stripe-js";
 import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
 import { loadStripe } from '@stripe/stripe-js';
+import CheckOutFrom from "./CheckOutFrom";
 
 //todo: add publish key
-const stripePromise = loadStripe('');
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_GATEWAY);
 
 const Payment = () => {
     return (
@@ -11,7 +12,7 @@ const Payment = () => {
             <SectionTitle heading="Payment" subHeading="Please Eat To Eat" />
             <div>
                 <Elements stripe={stripePromise}>
-
+                    <CheckOutFrom />
                 </Elements>
             </div>
 
